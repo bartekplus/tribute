f019a987a2fc9947daeac24824a11ceb6f445b25
Bartosz Tomczyk <bartekplus@gmail.com>
2023-06-24 12:27:57 +0200

    Update dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 import sass from "rollup-plugin-sass";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
